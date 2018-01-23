6429b77d6c54cfba40b33a50a510ac954ff3d5a3
Dominik Znidar <dominik@iryo.io>
2018-01-23 16:19:56 +0100

    Add initial eth requirements

--- config/config.go
+++ config/config.go
@@ -1,13 +1,19 @@
 package config
 
 import (
+	"crypto/ecdsa"
+	"encoding/hex"
+	"fmt"
+	"reflect"
+
 	"github.com/caarlos0/env"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 type Config struct {
-	IryoAddr       string `env:"IRYO_ADDR" envDefault:"iryo:80"`
-	EthPublic      string `env:"ETH_PUBLIC"`
-	EthPrivate     string `env:"ETH_PRIVATE"`
+	IryoAddr       string           `env:"IRYO_ADDR" envDefault:"iryo:80"`
+	EthPublic      string           `env:"ETH_PUBLIC"`
+	EthPrivate     ecdsa.PrivateKey `env:"ETH_PRIVATE,required"`
 	EncryptionKeys map[string][]byte
 	Connections    []string
 	Tokens         map[string]string
@@ -15,5 +21,25 @@ type Config struct {
 
 func New() (*Config, error) {
 	cfg := &Config{}
-	return cfg, env.Parse(cfg)
+	parsers := map[reflect.Type]env.ParserFunc{
+		reflect.TypeOf(cfg.EthPrivate): parseEthPrivateKey,
+	}
+	return cfg, env.ParseWithFuncs(cfg, parsers)
+}
+
+func parseEthPrivateKey(v string) (interface{}, error) {
+	bd, err := hex.DecodeString(v)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode private key from HEX; %v", err)
+	}
+	k, err := crypto.ToECDSA(bd)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to convert to private key; %v", err)
+	}
+
+	return *k, nil
+}
+
+func (c *Config) GetEthPublicAddress() string {
+	return crypto.PubkeyToAddress(c.EthPrivate.PublicKey).String()
 }
