ea930e2232ccd4f0f3cf8feb5834f1972c157c75
Mateusz Krasucki <mateusz.krasucki@3fs.si>
2018-11-07 15:18:26 +0100

    ADD ECG and HeartRate openEHR structs

--- openEHR/structs.go
+++ openEHR/structs.go
@@ -44,6 +44,34 @@ type BloodPressure struct {
 	Ts        string `json:"/content[openEHR-EHR-COMPOSITION.encounter.v1]/context/other_context/items[openEHR-EHR-OBSERVATION.blood_pressure.v1]/data[at0001]/events[at0006]:0/time,omitempty"`
 }
 
+type HeartRate struct {
+	Name  string `json:"/content[openEHR-EHR-COMPOSITION.encounter.v1]/context/other_context/items[openEHR-EHR-OBSERVATION.pulse.v1]/data[at0002]/events[at0003]:0/data[at0001]/items[at0004]|name,omitempty"`
+	Value string `json:"/content[openEHR-EHR-COMPOSITION.encounter.v1]/context/other_context/items[openEHR-EHR-OBSERVATION.pulse.v1]/data[at0002]/events[at0003]:0/data[at0001]/items[at0004],omitempty"`
+	Ts    string `json:"/content[openEHR-EHR-COMPOSITION.encounter.v1]/context/other_context/items[openEHR-EHR-OBSERVATION.pulse.v1]/data[at0002]/events[at0003]:0/time,omitempty"`
+}
+
+type ECG struct {
+	Shared
+	RRRate                  string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0013]/value,omitempty"`
+	PRInterval              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0012]/value,omitempty"`
+	QRSDuration             string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0014]/value,omitempty"`
+	QTInterval              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0007]/value,omitempty"`
+	QTCInterval             string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0008]/value,omitempty"`
+	PAxis                   string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0023]/items[at0020]/value,omitempty"`
+	QRSAxis                 string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0023]/items[at0021]/value,omitempty"`
+	TAxis                   string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0023]/items[at0022]/value,omitempty"`
+	PAmplitude              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0029]/items[at0041]/value,omitempty"`
+	SAmplitude              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0035]/items[at0053]/value,omitempty"`
+	RAmplitude              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0039]/items[at0050]/value,omitempty"`
+	AutomaticInterpretation string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0009]/value,omitempty"`
+	DeviceName              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/protocol[at0003]/items[at0076|openEHR-EHR-CLUSTER.device.v1]/items[at0001]/value,omitempty"`
+	DeviceType              string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/protocol[at0003]/items[at0076|openEHR-EHR-CLUSTER.device.v1]/items[at0003]/value,omitempty"`
+	DeviceManufacturer      string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/protocol[at0003]/items[at0076|openEHR-EHR-CLUSTER.device.v1]/items[at0004]/value,omitempty"`
+	AttachmentName          string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0083|openEHR-EHR-CLUSTER.multimedia.v0]/items[at0002]/value,omitempty"`
+	AttachmentType          string  `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0083|openEHR-EHR-CLUSTER.multimedia.v0]/items[at0001]/value|media_type,omitempty"`
+	AttachmentData          *string `json:"/content[openEHR-EHR-OBSERVATION.ecg_result.v0]/data[at0001]/events[at0002]/data[at0005]/items[at0083|openEHR-EHR-CLUSTER.multimedia.v0]/items[at0001]/value|data,omitempty"`
+}
+
 type VitalSignsFields struct {
 	Weight
 	Glucose
